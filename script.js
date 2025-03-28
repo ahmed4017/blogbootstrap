d7b26877961bb499a64004088d1339f861774347
Ahmed Rabea <90070993+ahmed4017@users.noreply.github.com>
2025-03-28 06:33:30 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('sidemenuicon').addEventListener('click', function() {
+    const menu = document.getElementById('menu');
+    menu.style.display = menu.style.display === 'flex' ? 'none' : 'flex';
+});
